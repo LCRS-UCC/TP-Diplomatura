46c43d5ba8ae6948293e8f4cb4d753133cd72210
MarkU <rmarku@gmail.com>
2014-10-14 19:28:55 -0300

    Implementado Blinky basico

--- main.c
+++ main.c
@@ -1,8 +1,67 @@
+#include "stm32f4xx.h"			// Header del micro
+#include "stm32f4xx_gpio.h"		// Perifericos de E/S
+#include "stm32f4xx_rcc.h"		// Para configurar el (Reset and clock controller)
 
-int main(void)
-{
+/* Simple funcion de delay */
+void Delay(__IO uint32_t nCount);
 
-    while(1)
-    {
-    }
+/* Inicializa los puertos E/S */
+void ES_Init();
+
+// Definiciones de hardware
+#define LED_V GPIO_Pin_12
+#define LED_N GPIO_Pin_13
+#define LED_R GPIO_Pin_14
+#define LED_A GPIO_Pin_15
+
+#define BOTON GPIO_Pin_0
+
+int main(void) {
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	// configuro GPIO
+	ES_Init();
+
+	while (1) {
+
+		GPIO_SetBits(GPIOD, LED_A);
+		Delay(0x1FFFFF);
+		GPIO_ResetBits(GPIOD, LED_A);
+		Delay(0x1FFFFF);
+
+		if (GPIO_ReadInputDataBit(GPIOA, BOTON) == 1) {
+			GPIO_SetBits(GPIOD, LED_N);
+		}
+	}
+}
+
+
+
+
+void ES_Init() {
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_15 | GPIO_Pin_14 | GPIO_Pin_13
+			| GPIO_Pin_12;
+
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP; // (Push/Pull)
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+	// Arranco el clock del periferico
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+void Delay(__IO uint32_t nCount) {
+	while (nCount--) {
+	}
 }
